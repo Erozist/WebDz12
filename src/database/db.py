7a623b05548ee95e903ae6d68ce8f08219b01893
Erozist <erozist87@gmail.com>
2024-06-13 21:16:31 +0300

    ADD_Alembic

--- src/database/db.py
+++ src/database/db.py
@@ -3,7 +3,6 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = f"postgresql+asyncpg://{os.environ.get('POSTGRES_USERNAME')}:{os.environ.get('POSTGRES_PASSWORD')}@localhost/dz11"
-# DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://username:password@localhost/dbname")
 
 
 engine = create_async_engine(DATABASE_URL, echo=True)
